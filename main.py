8d15e5765195693a94f37127c0d3e22de2df5d29
sashank27 <sashankmishra27@gmail.com>
2019-05-30 22:50:00 +0530

    display spot diagram from batch ray trace

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
 
     # Set up primary optical system
     cwd = os.getcwd()
-    file = "test.zmx"
+    file = "test2.zmx"
     testFile = cwd + "\\" + file
     TheSystem.LoadFile(testFile, False)
     print("File Imported")
@@ -111,6 +111,27 @@ if __name__ == '__main__':
     SurfaceList = []
     for i in range(TheLDE.NumberOfSurfaces):
         SurfaceList.append(TheLDE.GetSurfaceAt(i))
+
+    TheLDE.GetSurfaceAt(9).TiltDecenterData.BeforeSurfaceOrder = constants.TiltDecenterOrderType_Decenter_Tilt
+    # TheLDE.GetSurfaceAt(9).TiltDecenterData.BeforeSurfaceDecenterX = 2
+    # TheLDE.GetSurfaceAt(9).TiltDecenterData.AfterSurfaceDecenterX = -2
+    # TheLDE.GetSurfaceAt(9).TiltDecenterData.BeforeSurfaceDecenterY = -3
+    # TheLDE.GetSurfaceAt(9).TiltDecenterData.AfterSurfaceDecenterY = 3
+
+    cbData = TheLDE.GetSurfaceAt(11)
+    print(cbData.Thickness)
+    # print(cbData.MechanicalSemiDiameter)
+    print(cbData.Parameter1)
+
+    # print(help(TheLDE.GetSurfaceAt(9).TiltDecenterData))
+    # print(TheLDE.GetSurfaceAt(8).TiltDecenterData.DecenterX)
+    # print(TheLDE.GetSurfaceAt(9).TiltDecenterData.AfterSurfaceDecenterX)
+    # print(TheLDE.GetSurfaceAt(9).TiltDecenterData.BeforeSurfaceDecenterY)
+    # print(TheLDE.GetSurfaceAt(9).TiltDecenterData.AfterSurfaceDecenterY)
+
+    CoordinateBreakSurfaces = [8, 11, 12, 15, 16, 19]
+    for i in range(len(CoordinateBreakSurfaces), 2):
+        Surface = SurfaceList[i]
 
     # Spot Diagram Analysis Results
     spot = TheSystem.Analyses.New_Analysis(constants.AnalysisIDM_StandardSpot)
@@ -124,33 +145,95 @@ if __name__ == '__main__':
     base = CastTo(spot, 'IA_')
     base.ApplyAndWaitForCompletion()
     spot_results = base.GetResults()
-    print(dir(spot_results.SpotData))
-    print(help(spot_results.SpotData))
+    # print(dir(spot_results.SpotData))
+    # print(help(spot_results.SpotData))
     print('RMS radius: %6.3f' %
           (spot_results.SpotData.GetRMSSpotSizeFor(1, 1)))
     print('GEO radius: %6.3f' %
           (spot_results.SpotData.GetGeoSpotSizeFor(1, 1)))
 
-    # analIDM = []
-    # API_enum = list(constants.__dicts__[0].keys())
-    # for i in API_enum:
-    #     if i.find('AnalysisIDM_') != -1:
-    #         analIDM.append(constants.__dicts__[0].get(i))
-    #         print(constants.__dicts__[0].get(i), ':  ', i)
-    # analIDM.sort()
+    # Set up Batch Ray Trace
+    raytrace = TheSystem.Tools.OpenBatchRayTrace()
+    nsur = TheSystem.LDE.NumberOfSurfaces
+    max_rays = 30
+    normUnPolData = raytrace.CreateNormUnpol(
+        (max_rays + 1) * (max_rays + 1), constants.RaysType_Real, nsur)
 
-    # for k in analIDM:
-    #     a = TheSystem.Analyses.New_Analysis(k)
-    #     if a is None:
-    #         print('This analysis cannot be opened in ',
-    #               'Sequential Mode' if TheSystem.Mode == 0 else 'Non-Sequential Mode',
-    #               ': enumID ', k)
-    #         continue
-    #     print(a.GetAnalysisName, '\t', a.HasAnalysisSpecificSettings)
-    #     a.Close()
+    # Define batch ray trace constants
+    hx = 0.0
+    max_wave = TheSystem.SystemData.Wavelengths.NumberOfWavelengths
+    num_fields = TheSystem.SystemData.Fields.NumberOfFields
+    # print(max_wave, " ", num_fields)
+    hy_ary = np.array([0, 0.707, 1])
+
+    # Initialize x/y image plane arrays
+    x_ary = np.empty((num_fields, max_wave, ((max_rays + 1) * (max_rays + 1))))
+    y_ary = np.empty((num_fields, max_wave, ((max_rays + 1) * (max_rays + 1))))
+    # print("x_ary shape", x_ary.shape)
+    # print("x_ary shape", y_ary.shape)
+
+    # Determine maximum field in Y only
+    max_field = 0.0
+    for i in range(1, num_fields + 1):
+        if (TheSystem.SystemData.Fields.GetField(i).Y > max_field):
+            max_field = TheSystem.SystemData.Fields.GetField(i).Y
+
+    # print("Max field", max_field)
+    plt.rcParams["figure.figsize"] = (5, 5)
+    colors = ('k', 'g', 'r')
+    markers = ('+', 's', '^')
+
+    if TheSystem.SystemData.Fields.GetFieldType() == constants.FieldType_Angle:
+        field_type = 'Angle'
+    elif TheSystem.SystemData.Fields.GetFieldType() == constants.FieldType_ObjectHeight:
+        field_type = 'Height'
+    elif TheSystem.SystemData.Fields.GetFieldType() == constants.FieldType_ParaxialImageHeight:
+        field_type = 'Height'
+    elif TheSystem.SystemData.Fields.GetFieldType() == constants.FieldType_RealImageHeight:
+        field_type = 'Height'
+
+    # print("Field type", field_type)
+    field = 1
+    for wave in range(1, max_wave + 1):
+        # Adding Rays to Batch, varying normalised object height hy
+        normUnPolData.ClearData()
+        waveNumber = wave
+        # for i = 1:((max_rays + 1) * (max_rays + 1))
+        for i in range(1, (max_rays + 1) * (max_rays + 1) + 1):
+
+            px = np.random.random() * 2 - 1
+            py = np.random.random() * 2 - 1
+
+            while (px*px + py*py > 1):
+                py = np.random.random() * 2 - 1
+            normUnPolData.AddRay(
+                waveNumber, hx, hy_ary[field - 1], px, py, constants.OPDMode_None)
+
+        baseTool = CastTo(raytrace, 'ISystemTool')
+        baseTool.RunAndWaitForCompletion()
+
+        # Read batch raytrace and display results
+        normUnPolData.StartReadingResults()
+        output = normUnPolData.ReadNextResult()
+
+        while output[0]:                                                    # success
+            # ErrorCode & vignetteCode
+            if ((output[2] == 0) and (output[3] == 0)):
+                x_ary[field - 1, wave - 1, output[1] - 1] = output[4]   # X
+                y_ary[field - 1, wave - 1, output[1] - 1] = output[5]   # Y
+            output = normUnPolData.ReadNextResult()
+        temp = plt.plot(np.squeeze(x_ary[field - 1, wave - 1, :]), np.squeeze(
+            y_ary[field - 1, wave - 1, :]), '.', ms=3, c=colors[wave - 1],
+            marker=markers[wave - 1])
+
+    plt.title('Spot Diagram: %s' % (os.path.basename(testFile)))
+    plt.draw()
 
     '''This will clean up the connection to OpticStudio.
     Note that it closes down the server instance of OpticStudio,
     so you for maximum performance do not do this until you need to.'''
     del zosapi
     zosapi = None
+
+    # place plt.show() after clean up to release OpticStudio from memory
+    # plt.show()
